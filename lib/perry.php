98d701033db4a88644b037521392f6ea74d11106
rk <robert@voiceplex.net>
2011-02-07 13:31:15 -0500

    Added BSD license to framework file, and a copy in the root of the project.

--- lib/perry.php
+++ lib/perry.php
@@ -8,9 +8,38 @@
  * Perry uses REST verbs and static/regex routes to respond to various requests.
  * The URI matching is done first by static means, then by Regular Expressions.
  *
+ * LICENSE:
+ * Copyright (c) 2011, Robert Kosek
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are
+ * permitted provided that the following conditions are met:
+ *
+ *   1.) Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *
+ *   2.) Redistributions in binary form must reproduce the above copyright notice, this list of
+ *       conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *
+ *   3.) Neither the name of Robert Kosek nor the names of its contributors may be used to
+ *       endorse or promote products derived from this software without specific prior written
+ *       permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+ * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+ * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  * @author Robert Kosek <robert.kosek@thewickedflea.com>
- * @version 0.1
+ * @version 0.1.1
  * @copyright Robert Kosek, 5 February, 2011
+ * @license BSD License <license.txt>
  * @package Perry
  **/
 
@@ -211,7 +240,7 @@ class Response {
     return $result;
   }
   
-  public function view($template, Array $locals) {
+  public static function view($template, Array $locals) {
     ob_start();
     
     extract($locals);
